fbf678b4303fd79fd8ce3125d3e588ac4220e92e
Joshua Hansen <joshlha16@gmail.com>
2024-10-21 14:03:54 -0700

    Create basic swing scaffold.

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    alias(libs.plugins.kotlin)
+    alias(libs.plugins.serialization)
+    application
+}
+
+group = "io.github.joshlha"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(libs.miglayout)
+    implementation(libs.bundles.coroutines)
+    implementation(libs.bundles.flatlaf)
+    implementation(libs.swingx)
+    implementation(libs.serialization.json)
+    implementation(libs.jide.common)
+    implementation(libs.logback)
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain {
+        languageVersion = libs.versions.java.map(JavaLanguageVersion::of)
+        vendor = JvmVendorSpec.AZUL
+    }
+}
+
+application {
+    mainClass.set("io.github.joshlha.partnershipproject.Main")
+}
